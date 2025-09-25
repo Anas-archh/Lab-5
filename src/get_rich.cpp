f0e7d5eb5db1b49d86566aae9623fa8545449f1f
Sandesh Kumar <60325302+sandeshkumar003@users.noreply.github.com>
2025-09-25 16:45:35 +0500

    Delete src directory

--- src/get_rich.cpp
+++
@@ -1,8 +0,0 @@
-#ifndef GET_RICH
-#define GET_RICH
-
-/*
-Write your solution here.
-*/
-
-#endif  // GET_RICH
--- src/my_stack.cpp
+++
@@ -1,8 +0,0 @@
-#ifndef MY_STACK
-#define MY_STACK
-
-/*
-Write your solution here.
-*/
-
-#endif  // MY_STACK
